c8b057ae08d58f7b169b6dbe91997e4f6aa2eb0b
Andres Gonzalez <andres.gonconde@gmail.com>
2018-06-18 12:58:20 -0500

    More math, travis testing

--- ProEngine/proUtilities/Include/proMath.h
+++ ProEngine/proUtilities/Include/proMath.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "proPrerequisitesUtilities.h"
+#define M_PI 3.14159265359
 
 namespace proEngine {
   namespace Math {
@@ -25,7 +26,9 @@ namespace proEngine {
     };
 
     template <typename T>
-    float pow(T base, T exp) {};
+    float pow(T base, T exp) {
+    };
+
     template <typenname T>
      float sin(T n) {
        float Res = 0, temp = 0;
@@ -45,6 +48,40 @@ namespace proEngine {
          temp = Res;
          Res += (pow(-1, i))*(pow(n, 2 * i) / factorial(2 * i));
        } while ((Res - temp) > 0.00000001 || (Res - temp) < -0.00000001);
+       return Res;
+     };
+
+     template <typename T>
+     float tan(T n) {
+       Res = sin(n) / cos(n);
+       return Res;
+     };
+
+     template <typenname T>
+     float asin(T n) {
+       float Res = 0, temp = 0;
+       int i = 0;
+       do {
+         temp = Res;
+         Res += ((factorial(2 * i)) / (pow(4, i)*(pow(factorial(i), 2))*((2 * i) + 1)))*(pow(n, 2i + 1));
+       } while ((Res - temp) > 0.00000001 || (Res - temp) < -0.00000001);
+       return Res;
+     };
+
+     template <typename T>
+     float acos(T n) {
+       float Res = (M_PI/2);
+       return Res - asin(n);
+     };
+
+     template <typename T>
+     float atan(T n) {
+       float Res = 0, temp = 0;
+       int i = 0;
+       do {
+         temp = Res;
+         Res += (pow(-1, i) / ((2 * i) + 1)) * pow(n, (2 * i) + 1);
+       }((Res - temp) > 0.00000001 || (Res - temp) < -0.00000001);
        return Res;
      };
   }
